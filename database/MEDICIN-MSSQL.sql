825ae136117f5cd91dd892bafd57db80481d611c
Aksel Schmidt <aks@trifork.com>
2014-02-13 15:46:28 +0100

    tables and initial doc

---
+++ database/MEDICIN-MSSQL.sql
@@ -0,0 +1,22 @@
+CREATE TABLE [BASE].[T_HAI_MEDICIN] (
+        [V_REGION] [varchar](4) NULL,
+        [V_CPR] [varchar](10) NULL,
+        [V_SHAK] [varchar](10) NULL,
+        [D_ADM_START] [datetime] NULL,
+        [D_ORD_START] [datetime] NULL,
+        [D_ORD_SLUT] [datetime] NULL,
+        [D_KONTAKT_START] [datetime] NULL,
+        [D_KONTAKT_SLUT] [datetime] NULL,
+        [V_ADM_VEJ] [varchar](50) NULL,
+        [V_ADM_DOSIS] [varchar](50) NULL,
+        [V_ADM_DOSIS_ENHED] [varchar](50) NULL,
+        [V_ADM_VOLUMEN] [varchar](50) NULL,
+        [V_AD_VOLUMEN_ENHED] [varchar](50) NULL,
+        [V_DRUGID] [bigint] NULL,
+        [V_PRIM_ATC] [varchar](20) NULL,
+        [V_LAEGEMIDDELNAVN] [varchar](50) NULL,
+        [V_BEH_INDIC_KODE] [varchar](20) NULL,
+        [V_BEH_INDIC] [nvarchar](150) NULL,
+        [InsertRow_id] [bigint] IDENTITY(1,1) NOT NULL,
+        [c_source_file] [int] NOT NULL
+)
